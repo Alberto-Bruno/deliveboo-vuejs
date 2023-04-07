7b3d592058dce23647ab1f85b4914b0b9dc1bca7
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-04-07 17:00:11 +0200

    Add font awesome's icon in main.js

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+library.add(faArrowDown, faUtensils);
 import App from "./App.vue";
 
 const app = createApp(App);
-
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
 app.mount("#app");
