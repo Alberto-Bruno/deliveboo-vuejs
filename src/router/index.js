241cdc036f3f811006995aaba1d471457bac5138
AndreaMinelli <minelliandrea94@gmail.com>
2023-04-07 18:08:43 +0200

    Added query params in restaurants route

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,17 @@ import HomePage from "../pages/HomePage.vue";
 import RestaurantsPage from "../pages/RestaurantsPage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
 
-// import NotFoundPage from "../pages/NotFoundPage.vue";
-
 // creiamo il router e definiamo le rotte
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "home", component: HomePage },
-    { path: "/restaurants", name: "restaurants", component: RestaurantsPage },
+    {
+      path: "/restaurants",
+      name: "restaurants",
+      component: RestaurantsPage,
+      props: (route) => ({ query: route.query.types }),
+    },
 
     { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundPage },
   ],
