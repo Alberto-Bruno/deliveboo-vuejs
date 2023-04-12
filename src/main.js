cf57a34b08e5d1e5f401c71e0650949d2e6f6232
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-04-12 12:24:27 +0200

    Add icons in main.js

--- src/main.js
+++ src/main.js
@@ -9,9 +9,11 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 import { faBoltLightning } from "@fortawesome/free-solid-svg-icons";
-
+import { faApple } from "@fortawesome/free-brands-svg-icons";
+import { faGooglePlay } from "@fortawesome/free-brands-svg-icons";
+;
 import "bootstrap/dist/css/bootstrap.min.css";
-library.add(faArrowDown, faUtensils, faBoltLightning);
+library.add(faArrowDown, faUtensils, faBoltLightning, faApple, faGooglePlay);
 import App from "./App.vue";
 
 const app = createApp(App);
