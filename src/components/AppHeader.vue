0d5f54a0a04eead30fc2afd0c0a39a68cee7d0b4
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-04-13 12:42:17 +0200

    Add dashboard link to navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,15 +7,31 @@ export default {
 <template>
   <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
-      <RouterLink class="navbar-brand d-flex align-items-center" id="navbar" :to="{ name: 'home' }">
+      <RouterLink
+        class="navbar-brand d-flex align-items-center"
+        id="navbar"
+        :to="{ name: 'home' }"
+      >
         <figure class="m-0">
-          <img class="logo-nav position-static" src="../assets/img/icon-logo.jpg" alt="" />
+          <img
+            class="logo-nav position-static"
+            src="../assets/img/icon-logo.jpg"
+            alt=""
+          />
         </figure>
-        <span class="green-title fw-bold">Delive</span><span class="blue-title fw-bold">Boo</span>
+        <span class="green-title fw-bold">Delive</span
+        ><span class="blue-title fw-bold">Boo</span>
       </RouterLink>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="{{ __('Toggle navigation') }}"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
 
@@ -27,9 +43,18 @@ export default {
               Home
             </RouterLink>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://127.0.0.1:8000/dashboard"
+              >Dashboard</a
+            >
+          </li>
         </ul>
 
-        <a class="btn btn-success py-1 px-3 fs-5" href="http://127.0.0.1:8000/login">Login | Registrati</a>
+        <a
+          class="btn btn-success log-btn py-1 px-3"
+          href="http://127.0.0.1:8000/login"
+          >Login | Registrati</a
+        >
         <!-- Right Side Of Navbar -->
       </div>
     </div>
@@ -55,6 +80,10 @@ export default {
     position: relative;
     top: 8px;
     // margin-top: 15px;
+  }
+
+  .log-btn {
+    font-size: 17px;
   }
 }
 </style>
