1af2583008452d4560122018ff9269bdf74fb0f2
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-04-14 13:11:00 +0200

    Add PaymentForm's route in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
 import MenuPage from "../pages/MenuPage.vue";
+import PaymentPage from "../pages/PaymentForm.vue";
 
 // creiamo il router e definiamo le rotte
 const router = createRouter({
@@ -19,6 +20,11 @@ const router = createRouter({
       path: "/restaurants/:slug",
       name: "menu",
       component: MenuPage,
+    },
+    {
+      path: "/payment",
+      name: "payment",
+      component: PaymentPage,
     },
 
     { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundPage },
