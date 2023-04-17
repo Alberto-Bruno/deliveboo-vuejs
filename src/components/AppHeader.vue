a167efa9c67383c439a69d712da35e5cd7766d91
Alberto-Bruno <brunoalberto97@outlook.it>
2023-04-17 18:45:28 +0200

    hamburger-menu cell.png

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -34,16 +34,16 @@ export default {
             </RouterLink>
           </li>
           <!-- <li class="nav-item">
-                                                                <a class="nav-link" href="http://127.0.0.1:8000/dashboard"
-                                                                >Dashboard</a
-                                                                >
-                                                                </li> -->
+                                                                          <a class="nav-link" href="http://127.0.0.1:8000/dashboard"
+                                                                          >Dashboard</a
+                                                                          >
+                                                                          </li> -->
         </ul>
         <div class="d-flex gap-4">
-          <a class="btn btn-danger log-btn py-1 px-3" href="http://127.0.0.1:8000/login">
+          <a class="btn btn-warning log-btn py-1 px-3" href="http://127.0.0.1:8000/login">
             <font-awesome-icon icon="fa-solid fa-user"></font-awesome-icon>
           </a>
-          <RouterLink :to="{ name: 'checkout' }" class="btn btn-danger log-btn py-1 px-3 cart" href="#">
+          <RouterLink :to="{ name: 'checkout' }" class="btn btn-warning log-btn py-1 px-3 cart" href="#">
             <font-awesome-icon icon="fa-solid fa-cart-shopping"></font-awesome-icon>
             <div class="cart-badge d-flex justify-content-center align-items-center">
               <span>{{ getCartQuantity }}</span>
