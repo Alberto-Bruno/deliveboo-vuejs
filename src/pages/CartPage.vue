e5e09b1c95b10d6857420819e122e773fa6f9d5f
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-04-17 19:21:30 +0200

    Add logic to clearDish

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -1,4 +1,5 @@
 <script>
+import { store } from "../store.js";
 import axios from "axios";
 import Braintree from "../components/Braintree.vue";
 const apiBase = "http://127.0.0.1:8000/api/new-order";
@@ -22,6 +23,7 @@ export default {
         errors: {},
         showPayment: false,
     }),
+
     methods: {
         addQuantity(dish) {
             dish.quantity++;
@@ -55,6 +57,7 @@ export default {
                 }
             }
             localStorage.setItem('cartDishes', JSON.stringify(this.cartDishes));
+            store.cartQuantity = JSON.parse(localStorage.getItem("cartDishes")).length;
         },
 
         totalPrice() {
@@ -65,6 +68,49 @@ export default {
             }
             return total_price.toFixed(2);
         },
+
+        sendForm() {
+            this.form.payment_status = 1;
+            this.form.dishes = (JSON.parse(localStorage.getItem('cartDishes')));
+            this.form.total_price = this.totalPrice();
+            return console.log(this.form);
+            axios
+                .post(apiBase, this.form)
+                .then((res) => {
+                    console.log(res.data);
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+        },
+        goToPayments() {
+            if (this.orderFormCheck()) this.showPayment = true;
+        },
+        orderFormCheck() {
+            this.errors = {};
+            if (!this.form.firstname || !isNaN(this.form.firstname)) {
+                this.errors.firstname = "Il nome che hai inserito non è valido.";
+            }
+            if (!this.form.lastname || !isNaN(this.form.lastname)) {
+                this.errors.lastname = "Il cognome che hai inserito non è valido.";
+            }
+            if (!this.form.address || !isNaN(this.form.address)) {
+                this.errors.address = "L'indirizzo inserito non è valido.";
+            }
+            if (!this.form.phone || isNaN(this.form.phone)) {
+                this.errors.phone = "Il numero di telefono inserito non è valido.";
+            }
+            const validRegex =
+                /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
+            if (!this.form.email || !this.form.email.match(validRegex)) {
+                this.errors.email = "La mail che hai inserito non è valida!";
+            }
+            if (!this.form.delivery_time) {
+                this.errors.delivery_time = "Devi inserire un orario di consegna.";
+            }
+
+            return Object.keys(this.errors).length ? false : true;
+        },
     },
 
     mounted() {
@@ -74,46 +120,7 @@ export default {
 
     },
 
-    sendForm() {
-        this.form.payment_status = 1;
-        return console.log(this.form);
-        axios
-            .post(apiBase, this.form)
-            .then((res) => {
-                console.log(res.data);
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    },
-    goToPayments() {
-        if (this.orderFormCheck()) this.showPayment = true;
-    },
-    orderFormCheck() {
-        this.errors = {};
-        if (!this.form.firstname || !isNaN(this.form.firstname)) {
-            this.errors.firstname = "Il nome che hai inserito non è valido.";
-        }
-        if (!this.form.lastname || !isNaN(this.form.lastname)) {
-            this.errors.lastname = "Il cognome che hai inserito non è valido.";
-        }
-        if (!this.form.address || !isNaN(this.form.address)) {
-            this.errors.address = "L'indirizzo inserito non è valido.";
-        }
-        if (!this.form.phone || isNaN(this.form.phone)) {
-            this.errors.phone = "Il numero di telefono inserito non è valido.";
-        }
-        const validRegex =
-            /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
-        if (!this.form.email || !this.form.email.match(validRegex)) {
-            this.errors.email = "La mail che hai inserito non è valida!";
-        }
-        if (!this.form.delivery_time) {
-            this.errors.delivery_time = "Devi inserire un orario di consegna.";
-        }
 
-        return Object.keys(this.errors).length ? false : true;
-    },
 };
 </script>
 
@@ -246,78 +253,7 @@ export default {
             ></Braintree>
           </div>
         </form>
-      </div>
-      <div class="col-lg-4 col-md-12 my-5 my-sm-5 my-lg-0">
-        <div class="card shadow my-5">
-          <div class="card-header bg-header">
-            <h2 class="text-center text-white m-0">Riepilogo Ordine</h2>
-          </div>
-          <div class="card-body shadow">
-            <div class="flow pb-0 p-3">
-              <div>
-                <div>
-                  <div class="mb-3">
-                    <div>
-                      <div class="row g-0">
-                        <div class="col-md-4">
-                          <div>
-                            <img class="card-img-top rounded shadow" />
-                          </div>
-                          <div>
-                            <img
-                              class="card-img-top rounded shadow"
-                              src="https://artsmidnorthcoast.com/wp-content/uploads/2014/05/no-image-available-icon-6.png"
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                        <div class="col-md-8">
-                          <div class="card-body p-3">
-                            <p class="fw-semibold"><span>ragu</span></p>
-                            <p class="fw-semibold mb-0">
-                              Prezzo: <span>5 &euro;</span><span> x 2</span>
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-4">
-                        <div
-                          class="d-flex justify-content-between align-items-center my-3"
-                        >
-                          <button
-                            class="btn btn-sm indietro text-white fw-semibold"
-                          >
-                            <i class="fa-solid fa-minus"></i>
-                          </button>
-                          <span class="fw-semibold">2</span>
-                          <button
-                            class="btn btn-sm indietro text-white fw-semibold"
-                          >
-                            <i class="fa-solid fa-plus"></i>
-                          </button>
-                        </div>
-                      </div>
-                      <div class="col-8">
-                        <div
-                          class="d-flex justify-content-end align-items-center my-3"
-                        >
-                          <button
-                            class="btn btn-sm btn-danger text-white fw-semibold mx-2"
-                          >
-                            <i
-                              class="fa-solid fa-trash-can"
-                              title="Elimina"
-                            ></i>
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                    <hr class="border border-secondary border-1 opacity-75" />
-                  </div>
-                </div>
-            </div>  
+      </div>  
             <div class="col-lg-4 col-md-12 my-5 my-sm-5 my-lg-0">
                 <div class="card shadow my-5">
                     <div class="card-header bg-header" >
@@ -374,19 +310,15 @@ export default {
                         </div>
                     </div>
                     <div class="card-footer backg-body" >
+                        <div>
                             <p class="fw-semibold mb-0">Prezzo totale: <span>{{totalPrice()}} &euro;</span></p>
                         </div>
-                </div>
-              </div>
+                        <div class="d-flex justify-content-end align-items-center my-3">
+                            <button class="btn btn-sm btn-danger  text-white fw-semibold mx-2"><i class="fa-solid fa-trash-can" title="Elimina"></i></button>
+                        </div>
+                    </div>
             </div>
-          </div>
-          <div class="card-footer backg-body">
-            <p class="fw-semibold mb-0">
-              Prezzo totale: <span>50 &euro;</span>
-            </p>
-          </div>
         </div>
-      </div>
     </div>
   </div>
 </template>
