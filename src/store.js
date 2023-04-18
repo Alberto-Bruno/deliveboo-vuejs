2ab5fd3074b8c43a7dcbd5e2626915f358dabe61
AndreaMinelli <minelliandrea94@gmail.com>
2023-04-18 19:45:41 +0200

    Added function into Store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,18 @@
-import { reactive } from 'vue';
+import { reactive } from "vue";
+
+function setCartQuantity() {
+  if (localStorage.getItem("cartDishes")) {
+    const dishes = JSON.parse(localStorage.getItem("cartDishes"));
+    let quantity = 0;
+    dishes.forEach((dish) => {
+      quantity += dish.quantity;
+    });
+    return quantity;
+  }
+
+  return 0;
+}
 
 export const store = reactive({
-
-    cartQuantity: 0
+  cartQuantity: setCartQuantity(),
 });
